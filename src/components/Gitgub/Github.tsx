c2c02cf590a4cc624b5b034d278e38ee2d339653
Dmitrii <doronin_dima@mail.ru>
2022-05-03 20:34:58 +0300

    fetch

--- src/components/Gitgub/Github.tsx
+++ src/components/Gitgub/Github.tsx
@@ -12,30 +12,32 @@ type SearchResults = {
 
 const Github = () => {
 
-const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
-const [users, setUsers] = useState<SearchUserType>([])
+const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null);
+const [users, setUsers] = useState<SearchUserType[]>([]);
+const [tempSearch, setTempsearch] = useState('it-kamasutra');
 
 useEffect(() => {
-  console.log('useEffect-1');
+  // console.log('useEffect-1');
   if (selectedUser) {
     document.title = selectedUser.login
   }
 }, [selectedUser])
 
+const fetchData = (term: string) => {
+  axios.get<SearchResults>(`https://api.github.com/search/users?q=${term}`)
+        .then(res => setUsers(res.data.items))
+}
+
 useEffect(() => {
-  console.log('useEffect-2');
-  
-  axios.get<SearchResults>('https://api.github.com/search/users?q=it-kamasutra')
-       .then(res => setUsers(res.data.items))
-}, [])
-
-
+  fetchData(tempSearch)
+},[tempSearch])
 
   return ( 
     <div className={classes.container}>
     <div>
       <div>
-        <input placeholder='search'/> <button>find</button>
+        <input placeholder='search' value={tempSearch} onChange={(e) => setTempsearch(e.currentTarget.value)}/>
+        <button onClick={() => {}}>find</button>
       </div>
       <ul>
         { users.map(item => <li key={item.id} className={selectedUser === item ? classes.selected : ""} onClick={() => {setSelectedUser(item); document.title = item}}>{item.login}</li>)}
